9c94d874d38eab6f9538386dcc5626ee3f022608
Igris <igris@pop-os.localdomain>
2021-08-24 11:16:42 +0545

    Add Variable files

---
+++ Basic_JS/_01_variables.js
@@ -0,0 +1,8 @@
+alert("This is variable haru");
+let a = 10;
+let b = 20;
+alert("a = 10");
+alert("b = 20");
+alert("c chai aafai a + b");
+let c = a + b;
+alert("c = " + c);                                            
