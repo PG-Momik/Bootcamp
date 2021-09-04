db0c67c99a5cc8afc75de375c6ce3316e3535a18
PG-Momik <momik12339@gmail.com>
2021-09-04 14:00:26 +0545

    Add string Methods

---
+++ Basic_JS/_04_stringMethods.js
@@ -0,0 +1,22 @@
+//length
+let txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+console.log(txt.length);
+
+//slice
+let str = "Apple, Banana, Kiwi";
+console.log(str.slice(7, 13));   
+//slices from index 7 to index 13
+
+//substring
+let str2 = "Apple, Banana, Kiwi";
+console.log(str2.substring(15,20));
+
+//string replace
+let text2 = "Please visit Microsoft!";
+let newText = text2.replace("Microsoft", "W3Schools");
+console.log(newText);
+
+//toUpperCase
+let text3 = "Hello World!";      
+let text3a = text3.toUpperCase();
+console.log(text3a);
