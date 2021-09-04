04b10099ee637d01ea5e94552f41b4302633f823
PG-Momik <momik12339@gmail.com>
2021-09-04 20:06:19 +0545

    Add Random Number Generator

---
+++ Intermediate_JS/_01_random_num_gen.js
@@ -0,0 +1,11 @@
+// 0-0.999
+var decimal =  Math.random();
+console.log(decimal);
+
+//0-19
+var non_dec =   Math.floor(Math.random()*20);
+console.log(non_dec);
+
+//1 - 10:
+var again = Math.floor(Math.random() * 10) + 1;
+console.log(again);
