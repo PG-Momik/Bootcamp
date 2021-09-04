c951309dd84a5d1787f9ca2a71f10b8eeac0ed04
PG-Momik <momik12339@gmail.com>
2021-09-04 20:08:43 +0545

    Add operator

--- Intermediate_JS/_03_operator.js
+++ Intermediate_JS/_03_operator.js
@@ -1,14 +1,14 @@
 var a = 5;
 var b = 10;
 var c = 15;
-
+//AND OPERATOR
 if(b>5 || c<20){
     console.log("AND operator says all is fine.");
 }
 else{
     console.log("Not fine.");
 }
-
+//AND OR OPERATOR
 if(a%5 == 0 && b%5 == 0 && c%5 == 0){
     console.log("All nums divisible by 5.");
 }else{
