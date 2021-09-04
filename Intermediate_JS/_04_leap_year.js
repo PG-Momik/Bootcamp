7e17b258e0b9033f95672f9ba7a0f54f72cd977f
PG-Momik <momik12339@gmail.com>
2021-09-04 20:10:47 +0545

    Add Leap year

--- Intermediate_JS/_04_leap_year.js
+++ Intermediate_JS/_04_leap_year.js
@@ -3,8 +3,7 @@ btn.addEventListener("click", function(){
     var year  = document.getElementById("year").value;
     if(year%4 == 0){
         alert("Leap Year.");
-    }
-    else{
+    }else{
         alert("Not Leap Year.");
     }
 })
