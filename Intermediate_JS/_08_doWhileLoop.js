0e843b6404ef7fc816d61fcea31dbe45bdb18695
PG-Momik <momik12339@gmail.com>
2021-09-05 10:07:01 +0545

    Add Do while loop

---
+++ Intermediate_JS/_08_doWhileLoop.js
@@ -0,0 +1,7 @@
+var students = ["Ashesh", "Deepa", "Lakpa", "Momik", "Rashila", "Shreeju", "Suza"];
+var i = 0;
+
+do{
+    console.log("Roll No."+ (i+1) + " is " + students[i]);
+    i++;
+}while(i<7);
