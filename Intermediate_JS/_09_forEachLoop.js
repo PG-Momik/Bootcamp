c4ee9e5aca194768267e63aa7ddf0d707e074e59
PG-Momik <momik12339@gmail.com>
2021-09-05 10:07:23 +0545

    Add for each loop

---
+++ Intermediate_JS/_09_forEachLoop.js
@@ -0,0 +1,5 @@
+var students = ["Ashesh", "Deepa", "Lakpa", "Momik", "Rashila", "Shreeju", "Suza"];
+
+students.forEach(function(student, index, arr){
+     console.log("Roll No." + (index+1) + " is :" + student);
+})
