ea9cdf7fe65b690de9bdb89d46f99fdcf9cb80eb
PG-Momik <momik12339@gmail.com>
2021-09-05 10:06:29 +0545

    Add for loop

---
+++ Intermediate_JS/_06_forLoop.js
@@ -0,0 +1,5 @@
+var students = ["Ashesh", "Deepa", "Lakpa", "Momik", "Rashila", "Shreeju", "Suza"];
+var i;
+for(i = 0; i< students.length; i++){
+    console.log("Roll No." + (i+1) + " is "+ students[i]);
+}
